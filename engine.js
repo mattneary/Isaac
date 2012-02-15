f59293b7bfa0c3aa6434e3d2bf21b1716339b58e
Matt Neary <neary.matt@gmail.com>
2012-02-15 18:05:52 -0500

    Collision fixed, Arrow keys added, Gravity added

--- engine.js
+++ engine.js
@@ -1,3 +1,7 @@
+keyCodes = {
+	up: '&', left: '%', right: "'", down: '('
+};
+
 //Keyboard functions
 var kkeys 	= {},
 	isDown 	= function(char) {
@@ -25,7 +29,7 @@ Object.prototype.extend = function(/* variable number of arguments */) {
 };				
 
 //Main function	
-var go = function(characters) {				
+var go = function(characters, callback) {				
 	var t = 0;
 	handleKeyPresses();
 	int = setInterval(function() {
@@ -33,6 +37,8 @@ var go = function(characters) {
 		characters.move(t), characters.draw(t);
 		t++;				
 	}, 30);
+	
+	callback && callback();
 };		var Rect = function(options) {				
 	var color = options.color || "#000", 
 		x = options.x, 
@@ -63,7 +69,7 @@ var go = function(characters) {
 		x: 0,
 		y: 0
 	};								
-	this.collisions = collisions;	
+	this.collisions = collisions;
 		
 	this.text = parameters.text;			
 		
@@ -90,28 +96,48 @@ Character.prototype = {
 				ctx.font         = 'italic 30px sans-serif';
 				ctx.textBaseline = 'top';
 				if( text )
-					console.log("Filling"), ctx.fillText(text, rect.x+offset.x, height-(rect.y+offset.y));
+					/*console.log("Filling"), */ctx.fillText(text, rect.x+offset.x, height-(rect.y+offset.y));
 			}
 		})(this.shapes, this.offset);
 	},
-	move: function(t) {
+	move: function(t,gravity) {
 		//Speed is a function of time -- more natural than position
-		this.offset.x += this.speed.x.call(this, t);
-		this.offset.y += this.speed.y.call(this, t);
+		var deduction;
+		if( this.gravityStart )
+			deduction = t-(this.gravityStart*gravity);
+			
+		var xSpeed = this.speed.x.call(this, t),
+			ySpeed = this.speed.y.call(this, t);
+		ySpeed = deduction?ySpeed-deduction:(
+			t==this.gravityStart?ySpeed:0
+		);
+		if( this.fixed ) {
+			console.log("FIXED");
+			this.offset.x += this.speed.x.call(this, t);
+			this.offset.y += this.speed.y.call(this, t);
+		} else {
+			this.offset.x += xSpeed;
+			this.offset.y += ySpeed;
+		}
+	},
+	gravityStart: null,
+	gravityReset: function() {
+		this.gravityStart = null;
 	},
 	isOccupied: function(x,y) {
 		for( var k in this.shapes ) {
 			var shape = this.shapes[k],
-				pastx = x + (shape.width  || shape.size),
-				pasty = y + (shape.height || shape.size);							
+				pastx = x,
+				pasty = y;							
 		
 		//Check if x is in range [offset.x + shape.x, offset.x + shape.x + size]
 		//Check if y is in range [offset.y + shape.y, offset.y + shape.y + size]
 		
-		var error = 2;
+		var error = 0;				
+		
 		if(
-				(pastx >= (this.offset.x+shape.x-error) && pastx <= (this.offset.x+shape.x)+shape.width+error) &&
-				(pasty >= (this.offset.y+shape.y-error) && pasty <= (this.offset.y+shape.y)+shape.height+error)
+				(pastx >= (this.offset.x+shape.x)-error && pastx <= (this.offset.x+shape.x)+shape.width+error) &&
+				(pasty <= (this.offset.y+shape.y)-error && pasty >= (this.offset.y+shape.y)-shape.height+error)
 			) return true;
 		}
 		return false;
@@ -120,8 +146,8 @@ Character.prototype = {
 		for( var k in this.shapes ) {
 			if( !character || !character.shapes ) continue;
 			var shape  = character.shapes[k],
-				offset = character.offset;					
-			
+				offset = character.offset;											
+						
 			//Check corners for overlap
 			if(		(this.isOccupied(offset.x+shape.x, offset.y+shape.y)) ||
 					(this.isOccupied(offset.x+shape.x+(shape.width||shape.size), offset.y+shape.y)) ||
@@ -146,16 +172,17 @@ Character.prototype = {
 CharacterList.prototype.extend(Array.prototype, {
 	move: function(t) {
 		var characters = this;
-		for( var k in characters ) {
+		for( var k in characters ) {		
 			character = characters[k];					
-			character && character.move && character.move(t);					
+			character && character.move && character.move(t,characters.gravity);					
 			
 			var collisions = character.collisions;
 			for( var k in collisions ) {
-				var collision = collisions[k];
-				
-				if( characters[k] && characters[k].overlaps )
-				character && character.overlaps && character.overlaps(characters[k]) || characters[k].overlaps && characters[k].overlaps(character) && collision();
+				if( k == "extend" ) continue;
+				var collision = collisions[k];				
+
+				if( characters[k].overlaps && (character.overlaps(characters[k]) || characters[k].overlaps(character)) )
+					collision();
 			}
 		}
 	},
@@ -165,5 +192,6 @@ CharacterList.prototype.extend(Array.prototype, {
 			character = characters[k];					
 			character && character.shade && character.shade(t);
 		}
-	}
+	},
+	gravity: 1
 });
